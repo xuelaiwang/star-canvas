e700f3f93557b8bf73e3b4891ca1a12e5a90ddbf
wangxuelai <466625985@qq.com>
2019-09-11 19:27:24 +0800

    '官网最新代码提交'

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $(function () {
     $('.three-tab .tab-item').click(function () {
         var index =  $('.three-tab .tab-item').index(this);
         $(this).addClass('tab-active').siblings().removeClass('tab-active');
-        $('.three-content').eq(index).addClass('show').siblings('.three-content').removeClass('show');
+        $('.three-content-right .themedesc').eq(index).removeClass('hide').addClass('show').siblings('.themedesc').removeClass('show').addClass('hide');
+        $('.three-content-left img').eq(index).removeClass('hide').addClass('show').siblings('img').removeClass('show').addClass('hide');
     })
 });
